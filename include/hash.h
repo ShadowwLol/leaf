1b9bb743fc76f7dc383d91f53f2403c0d1dd3596
ShadowwLol <shadowwlol613@protonmail.com>
2022-03-01 16:47:50 +0000

    sha512 added

--- include/hash.h
+++ include/hash.h
@@ -5,8 +5,10 @@
 
 #include <openssl/sha.h>
 
+#define SHA512_OUT_LENGTH (129)
 #define SHA256_OUT_LENGTH (65)
 
+void sha512_string(char *string, char * outputBuffer);
 void sha256_string(char *string, char * outputBuffer);
 
 #endif /* _HASH_H */
--- src/hash.c
+++ src/hash.c
@@ -1,5 +1,19 @@
 #include "../include/hash.h"
 
+void sha512_string(char *string, char * outputBuffer){
+	unsigned char hash[SHA512_DIGEST_LENGTH];
+	SHA512_CTX sha512;
+	SHA512_Init(&sha512);
+	SHA512_Update(&sha512, string, MEL_strlen(string));
+	SHA512_Final(hash, &sha512);
+
+	for(size_t i = 0; i < SHA512_DIGEST_LENGTH; i++){
+		sprintf(outputBuffer + (i * 2), "%02x", hash[i]);
+	}
+
+	outputBuffer[SHA512_OUT_LENGTH-1] = 0;
+}
+
 void sha256_string(char *string, char * outputBuffer){
 	unsigned char hash[SHA256_DIGEST_LENGTH];
 	SHA256_CTX sha256;
